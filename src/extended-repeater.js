4ec074c64bf0deb0c0702166569aa392f0bccac4
airMay-hub <kiiikaaa.93@mail.ru>
2021-09-15 19:19:24 +0300

    feat: solve extended-repeater task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,7 +15,28 @@ import { NotImplementedError } from '../extensions/index.js';
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-export default function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function repeater(str, options) {
+  let x = '';
+	if(options.addition !== undefined && typeof(options.addition) !== 'string') {
+		options.addition = '' + options.addition;
+	}
+	options.repeatTimes = options.repeatTimes || 1;
+	for(let i = 0; i < options.repeatTimes; i++) {
+		x += str;
+		options.additionRepeatTimes = options.additionRepeatTimes || 1;
+		for(let j = 0; j < options.additionRepeatTimes; j++) {
+			options.addition = options.addition || '';
+			x += options.addition;
+			if (j < options.additionRepeatTimes - 1) {
+				options.additionSeparator = options.additionSeparator || '|';
+				x += options.additionSeparator;
+			}
+		}
+		options.separator = options.separator || '+';
+		if(i < options.repeatTimes-1) {
+			options.separator = options.separator || '|';
+			x += options.separator;
+		}
+	}
+	return x;
 }
