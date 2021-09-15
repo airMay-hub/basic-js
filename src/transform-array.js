5bc35f2c5c2aaae988c0f0d597c9d55205914712
airMay-hub <kiiikaaa.93@mail.ru>
2021-09-15 18:42:21 +0300

    feat: solve transform-array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,7 +13,48 @@ import { NotImplementedError } from '../extensions/index.js';
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-export default function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function transform(arr) {
+  
+  if (arr instanceof Array === false) {
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }
+
+  if (arr.length == 0) {
+    return arr;
+  }
+
+  let copy = arr.slice();
+
+  for (let i = 0; i < copy.length; i++) {
+
+    if(copy[i] === '--discard-next' && 
+      (copy[i + 2] === '--double-prev' || 
+      copy[i + 2] === '--discard-prev')) {
+        copy.splice(i, 3);
+        
+    } else if ((copy[i] === '--discard-prev' || 
+              copy[i] === '--double-prev') 
+              && i === 0) {
+                copy.splice(i, 1)
+
+    } else if ((copy[i] === '--discard-next' || 
+              copy[i] === '--double-next') && 
+              i === copy.length - 1) {
+                copy.splice(i, 1)
+
+    } else if (copy[i] === '--discard-prev') {
+        copy.splice(i - 1, 2);
+        
+    } else if (copy[i] === '--double-prev') {
+        copy.splice(i, 1, copy[i - 1]);
+
+    } else if (copy[i] === '--double-next') {
+        copy.splice(i, 1, copy[i + 1]);
+
+    } else if (copy[i] === '--discard-next') {
+        copy.splice(i, 2);
+    }
+  }
+
+  return copy;
 }
